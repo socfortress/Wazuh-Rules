07279b60a10caa5b76592a6296f139a650d70789
SOCFortress <95670863+socfortress@users.noreply.github.com>
2022-08-20 09:44:32 -0500

    Update windowsfirewall.ps1

--- Active Response/Windows/windowsfirewall.ps1
+++ Active Response/Windows/windowsfirewall.ps1
@@ -11,7 +11,8 @@ $ErrorActionPreference = "SilentlyContinue"
 $hostip = (Get-WmiObject -Class Win32_NetworkAdapterConfiguration | where {$_.DHCPEnabled -ne $null -and $_.DefaultIPGateway -ne $null}).IPAddress | Select-Object -First 1
 $malicious_ip = ($INPUT_ARRAY."parameters"."alert"."cmd").ToString()
 
-#Resolve Malicious Domain to Localhost
+if ( $malicious_ip -ne '127.0.0.1' -And $malicious_ip -ne '0.0.0.0' -And $malicious_ip -ne $hostip )
+{
 netsh advfirewall firewall add rule name="Wazuh Block $malicious_ip" dir=out interface=any action=block remoteip=$malicious_ip/32
-
 echo  "Malicious IP: $malicious_ip has been added to the block list" | ConvertTo-Json -Compress | Out-File -width 2000 C:\"Program Files (x86)"\ossec-agent\active-response\active-responses.log -Append -Encoding ascii
+}
