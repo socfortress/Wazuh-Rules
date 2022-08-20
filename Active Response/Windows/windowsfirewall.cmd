8aadda00a705920710bc42588602130f2b810694
SOCFortress <95670863+socfortress@users.noreply.github.com>
2022-08-20 09:40:22 -0500

    Create windowsfirewall.cmd

---
+++ Active Response/Windows/windowsfirewall.cmd
@@ -0,0 +1,8 @@
+:: Simple script to run Windows Firewall Block
+:: The script executes a powershell script and appends output.
+@ECHO OFF
+ECHO.
+
+pwsh.exe -executionpolicy ByPass -File "C:\Program Files (x86)\ossec-agent\active-response\bin\windowsfirewall.ps1"
+
+:Exit
