97a0712b7b3a0845b099fcf3b0f1f269778fa3c3
taylor_socfortress <111797488+taylorwalton@users.noreply.github.com>
2023-08-21 10:46:12 -0500

    Update wazuh_socfortress_rules.sh

--- wazuh_socfortress_rules.sh
+++ wazuh_socfortress_rules.sh
@@ -138,6 +138,7 @@ cloneRules() {
             find /var/ossec/etc/rules/ -name 'auditd_decoders.xml' -exec mv {} /var/ossec/etc/decoders/ \;
             find /var/ossec/etc/rules/ -name 'naxsi-opnsense_decoders.xml' -exec mv {} /var/ossec/etc/decoders/ \;
             find /var/ossec/etc/rules/ -name 'maltrail_decoders.xml' -exec mv {} /var/ossec/etc/decoders/ \;
+            find /var/ossec/etc/rules/ -name 'decoder-manager-logs.xml' -exec mv {} /var/ossec/etc/decoders/ \;
             /var/ossec/bin/wazuh-control info 2>&1 | tee /tmp/version.txt
             chown wazuh:wazuh /var/ossec/etc/rules/*
             chmod 660 /var/ossec/etc/rules/*
@@ -161,6 +162,7 @@ cloneRules() {
             find /var/ossec/etc/rules/ -name 'auditd_decoders.xml' -exec mv {} /var/ossec/etc/decoders/ \;
             find /var/ossec/etc/rules/ -name 'naxsi-opnsense_decoders.xml' -exec mv {} /var/ossec/etc/decoders/ \;
             find /var/ossec/etc/rules/ -name 'maltrail_decoders.xml' -exec mv {} /var/ossec/etc/decoders/ \;
+            find /var/ossec/etc/rules/ -name 'decoder-manager-logs.xml' -exec mv {} /var/ossec/etc/decoders/ \;
             /var/ossec/bin/wazuh-control info 2>&1 | tee /tmp/version.txt
             chown wazuh:wazuh /var/ossec/etc/rules/*
             chmod 660 /var/ossec/etc/rules/*
