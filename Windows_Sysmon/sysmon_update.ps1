19810025e780a2e30745c05c0c8eef6e8404d746
ventra007 <39557783+ventra007@users.noreply.github.com>
2024-03-07 11:24:50 +0200

    Update sysmon_update.ps1
    remove redundant variable

--- Windows_Sysmon/sysmon_update.ps1
+++ Windows_Sysmon/sysmon_update.ps1
@@ -8,5 +8,4 @@ $sysmonconfig_file = 'sysmonconfig-export.xml'
 $OutPath = $env:TMP
 $output = $sysinternals_zip
 Invoke-WebRequest -Uri $sysmonconfig_downloadlink -OutFile $OutPath\$sysmonconfig_file
-$serviceName = 'Sysmon64'
 Start-Process -FilePath $sysinternals_folder\Sysmon64.exe -Argumentlist @("-c", "$OutPath\$sysmonconfig_file") -Verb runAs
