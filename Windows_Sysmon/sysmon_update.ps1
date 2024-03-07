97e55c048c4da64aff752d40e83a537149fcb90c
ventra007 <39557783+ventra007@users.noreply.github.com>
2024-03-07 11:23:00 +0200

    Update sysmon_update.ps1

--- Windows_Sysmon/sysmon_update.ps1
+++ Windows_Sysmon/sysmon_update.ps1
@@ -1,5 +1,10 @@
-$sysmonconfig_downloadlink = 'https://raw.githubusercontent.com/SwiftOnSecurity/sysmon-config/master/sysmonconfig-export.xml'
+$sysinternals_folder = 'C:\Program Files\sysinternals'
+$sysinternals_zip = 'SysinternalsSuite.zip'
+$sysmonconfig_downloadlink = 'https://raw.githubusercontent.com/ventra007/sysmon-config/master/sysmonconfig-export.xml'
 $sysmonconfig_file = 'sysmonconfig-export.xml'
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
 $OutPath = $env:TMP
 $output = $sysinternals_zip
 Invoke-WebRequest -Uri $sysmonconfig_downloadlink -OutFile $OutPath\$sysmonconfig_file
